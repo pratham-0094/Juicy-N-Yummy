2500f275a6fde5bf3e315becd5e8c9bafda75f4c
pratham-0094 <sahupratham022003@gmail.com>
2022-05-12 13:28:32 +0530

    added auth model

---
+++ Backend/models/admin.js
@@ -0,0 +1,46 @@
+const { Schema } = require("mongoose");
+
+const Duration = new Schema({ time: number });
+const Admin = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+  },
+  phone_no: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  restaurant: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  distict: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  landmark: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: [Duration],
+  },
+});
+
+const admin = mongoose.model("Admin", Admin);
+module.exports = admin;
---
+++ Backend/models/users.js
@@ -0,0 +1,22 @@
+const { Schema } = require("mongoose");
+
+const Users = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+  },
+  phone_no: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const users = mongoose.model("Users", Users);
+module.exports = users;
