44b16f854f90b6441bf5546733ba85538a8d66dc
prakhar-5447 <sahuprakhar022003@gmail.com>
2022-05-12 20:10:07 +0530

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './user/home/home.component';
+import { LoginComponent } from './user/login/login.component';
+import { SignupComponent } from './user/signup/signup.component';
+import { NavbarComponent } from './user/navbar/navbar.component';
+import { AdminsignupComponent } from './admin/adminsignup/adminsignup.component';
+import { AdminloginComponent } from './admin/adminlogin/adminlogin.component';
+import { AdminnavbarComponent } from './admin/adminnavbar/adminnavbar.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, LoginComponent, SignupComponent, NavbarComponent, AdminsignupComponent, AdminloginComponent, AdminnavbarComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
