0bf6dec6fa31f9f951fdb51f4672f0bf595d3845
pratham-0094 <sahupratham022003@gmail.com>
2022-05-27 11:30:18 +0530

    user item panel

--- src/app/user/order/order.component.ts
+++ src/app/user/order/order.component.ts
@@ -1,4 +1,10 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { adminProfile } from 'src/app/model/adminProfile';
+import { items } from 'src/app/model/items';
+import { AdminAuthService } from 'src/app/service/admin-auth.service';
+import { AdminServiceService } from 'src/app/service/admin-service.service';
 
 @Component({
   selector: 'app-order',
@@ -6,87 +12,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order.component.css'],
 })
 export class OrderComponent implements OnInit {
-  constructor() {}
+  item!: items[];
+  category!: any;
+  id!: String;
+
+  constructor(
+    private adminServiceAuth: AdminServiceService,
+    private adminAuth: AdminAuthService,
+    private location: Location,
+    private route: ActivatedRoute
+  ) {
+    if (this.location.path() === '/admin/dashboard/order') {
+      this.adminAuth.getAdmin().subscribe((data: adminProfile) => {
+        this.category = data.category;
+        this.id = data._id;
+        this.adminServiceAuth.getItems(this.id).subscribe((res: items[]) => {
+          this.item = res;
+        });
+      });
+    } else {
+      this.adminAuth.getAdmin().subscribe((data: adminProfile) => {
+        this.category = data.category;
+      });
+      this.route.params.subscribe((params) => {
+        this.id = params['id'];
+        this.adminServiceAuth.getItems(this.id).subscribe((res: items[]) => {
+          this.item = res;
+        });
+      });
+    }
+  }
+
+  remove(id: String) {
+    this.adminServiceAuth.removeItems(id).subscribe((res) => {
+      console.log(res);
+      this.adminServiceAuth.getItems(this.id).subscribe((res: items[]) => {
+        this.item = res;
+      });
+    });
+  }
 
   ngOnInit(): void {}
-
-  items = [
-    {
-      food: 'Pizza',
-      rating: 4,
-      image:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTooPlWSljxJsQv2hyv2JgVd-Ozk0lA4WmV2A&usqp=CAU',
-      price: 30,
-    },
-    {
-      food: 'Pizza',
-      rating: 4,
-      image:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTooPlWSljxJsQv2hyv2JgVd-Ozk0lA4WmV2A&usqp=CAU',
-      price: 30,
-    },
-    {
-      food: 'Pizza',
-      rating: 4,
-      image:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTooPlWSljxJsQv2hyv2JgVd-Ozk0lA4WmV2A&usqp=CAU',
-      price: 30,
-    },
-    {
-      food: 'Pizza',
-      rating: 4,
-      image:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTooPlWSljxJsQv2hyv2JgVd-Ozk0lA4WmV2A&usqp=CAU',
-      price: 30,
-    },
-    {
-      food: 'Pizza',
-      rating: 4,
-      image:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTooPlWSljxJsQv2hyv2JgVd-Ozk0lA4WmV2A&usqp=CAU',
-      price: 30,
-    },
-    {
-      food: 'Pizza',
-      rating: 4,
-      image:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTooPlWSljxJsQv2hyv2JgVd-Ozk0lA4WmV2A&usqp=CAU',
-      price: 30,
-    },
-    {
-      food: 'Pizza',
-      rating: 4,
-      image:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTooPlWSljxJsQv2hyv2JgVd-Ozk0lA4WmV2A&usqp=CAU',
-      price: 30,
-    },
-    {
-      food: 'Pizza',
-      rating: 4,
-      image:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTooPlWSljxJsQv2hyv2JgVd-Ozk0lA4WmV2A&usqp=CAU',
-      price: 30,
-    },
-    {
-      food: 'Pizza',
-      rating: 4,
-      image:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTooPlWSljxJsQv2hyv2JgVd-Ozk0lA4WmV2A&usqp=CAU',
-      price: 30,
-    },
-    {
-      food: 'Pizza',
-      rating: 4,
-      image:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTooPlWSljxJsQv2hyv2JgVd-Ozk0lA4WmV2A&usqp=CAU',
-      price: 30,
-    },
-    {
-      food: 'Pizza',
-      rating: 4,
-      image:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTooPlWSljxJsQv2hyv2JgVd-Ozk0lA4WmV2A&usqp=CAU',
-      price: 30,
-    },
-  ];
 }
