a8223da21687bee735cd51b7e2a249a827655d37
pratham-0094 <sahupratham022003@gmail.com>
2022-05-27 22:25:20 +0530

    user dashboard change

--- src/app/user/order/order.component.ts
+++ src/app/user/order/order.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { adminProfile } from 'src/app/model/adminProfile';
 import { items } from 'src/app/model/items';
 import { AdminAuthService } from 'src/app/service/admin-auth.service';
+import { CartServiceService } from 'src/app/service/cart-service.service';
 import { ItemServiceService } from 'src/app/service/item-service.service';
 import { UserAuthService } from 'src/app/service/user-auth.service';
 
@@ -17,22 +18,36 @@ export class OrderComponent implements OnInit {
   id!: String;
 
   constructor(
-    private userServiceAuth: ItemServiceService,
+    private itemService: ItemServiceService,
+    private cartService: CartServiceService,
     private userAuth: UserAuthService,
     private route: ActivatedRoute
   ) {
-    this.route.paramMap.subscribe(params => { 
-      this.id = params.get('id') ?? ""; 
-      console.log(this.id);
-      
-  });    
-    
+    this.route.paramMap.subscribe((params) => {
+      this.id = params.get('id') ?? '';
+    });
     this.userAuth.getAdminId(this.id).subscribe((data: adminProfile) => {
       this.category = data.category;
     });
-    this.userServiceAuth.getItems(this.id).subscribe((res: items[]) => {
+    this.itemService.getItems(this.id).subscribe((res: items[]) => {
       this.item = res;
     });
+  }
+
+  addToCart(k: any) {
+    this.cartService.add(this.item[k]);
+  }
+
+  filter(name: String) {
+    if (name === 'All') {
+      this.itemService.getItems(this.id).subscribe((res: items[]) => {
+        this.item = res;
+      });
+    } else {
+      this.itemService.getItems(this.id).subscribe((res) => {
+        this.item = res.filter((res) => res.category === name);
+      });
+    }
   }
 
   ngOnInit(): void {}
