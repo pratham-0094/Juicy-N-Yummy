20cff9ef40616868c877dc4f4ebc4c7c1a93a0ed
pratham-0094 <sahupratham022003@gmail.com>
2022-05-27 11:28:36 +0530

    admin data fetch

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,9 +1,13 @@
+import { _RecycleViewRepeaterStrategy } from '@angular/cdk/collections';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { EditadminComponent } from 'src/app/dialog/editadmin/editadmin.component';
 import { adminProfile } from 'src/app/model/adminProfile';
+import { items } from 'src/app/model/items';
+import { review } from 'src/app/model/review';
 import { AdminAuthService } from 'src/app/service/admin-auth.service';
+import { AdminServiceService } from 'src/app/service/admin-service.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -16,21 +20,26 @@ export class DashboardComponent implements OnInit {
 
   profile!: any;
   now!: Number;
+  count!: Number;
 
   constructor(
     public router: Router,
     private adminAuth: AdminAuthService,
+    private adminServiceAuth: AdminServiceService,
     public dialog: MatDialog
   ) {
     this.adminAuth.getAdmin().subscribe((data: adminProfile) => {
-      this.profile = {
-        restaurant: data.restaurant,
-        category: data.category,
-        address: data.address,
-        duration: data.duration,
-        status: data.status,
-        Reviews: 23,
-      };
+      this.adminServiceAuth.getReview(data._id).subscribe((res: review[]) => {
+        this.count = res.length;
+        this.profile = {
+          restaurant: data.restaurant,
+          category: data.category,
+          address: data.address,
+          duration: data.duration,
+          status: data.status,
+          Reviews: this.count,
+        };
+      });
     });
     this.now = new Date().getHours();
   }
