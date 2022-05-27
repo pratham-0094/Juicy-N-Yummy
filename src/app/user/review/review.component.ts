939c1ff730d5a28177bbf2f239f320f561789642
pratham-0094 <sahupratham022003@gmail.com>
2022-05-27 11:30:05 +0530

    user review panel

--- src/app/user/review/review.component.ts
+++ src/app/user/review/review.component.ts
@@ -1,4 +1,10 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { adminProfile } from 'src/app/model/adminProfile';
+import { review } from 'src/app/model/review';
+import { AdminAuthService } from 'src/app/service/admin-auth.service';
+import { AdminServiceService } from 'src/app/service/admin-service.service';
 
 @Component({
   selector: 'app-review',
@@ -6,136 +12,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./review.component.css'],
 })
 export class ReviewComponent implements OnInit {
-  review = [
-    {
-      name: 'Pizza',
-      rating: 4.7,
-      image:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTooPlWSljxJsQv2hyv2JgVd-Ozk0lA4WmV2A&usqp=CAU',
-      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, recusandae?',
-    },
-    {
-      name: 'Pizza',
-      rating: 4.7,
-      image:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTooPlWSljxJsQv2hyv2JgVd-Ozk0lA4WmV2A&usqp=CAU',
-      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, recusandae?',
-    },
-    {
-      name: 'Pizza',
-      rating: 4.7,
-      image:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTooPlWSljxJsQv2hyv2JgVd-Ozk0lA4WmV2A&usqp=CAU',
-      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, recusandae?',
-    },
-    {
-      name: 'Pizza',
-      rating: 4.7,
-      image:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTooPlWSljxJsQv2hyv2JgVd-Ozk0lA4WmV2A&usqp=CAU',
-      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, recusandae?',
-    },
-    {
-      name: 'Pizza',
-      rating: 4.7,
-      image:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTooPlWSljxJsQv2hyv2JgVd-Ozk0lA4WmV2A&usqp=CAU',
-      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, recusandae?',
-    },
-    {
-      name: 'Pizza',
-      rating: 4.7,
-      image:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTooPlWSljxJsQv2hyv2JgVd-Ozk0lA4WmV2A&usqp=CAU',
-      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, recusandae?',
-    },
-    {
-      name: 'Pizza',
-      rating: 4.7,
-      image:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTooPlWSljxJsQv2hyv2JgVd-Ozk0lA4WmV2A&usqp=CAU',
-      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, recusandae?',
-    },
-    {
-      name: 'Pizza',
-      rating: 4.7,
-      image:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTooPlWSljxJsQv2hyv2JgVd-Ozk0lA4WmV2A&usqp=CAU',
-      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, recusandae?',
-    },
-    {
-      name: 'Pizza',
-      rating: 4.7,
-      image:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTooPlWSljxJsQv2hyv2JgVd-Ozk0lA4WmV2A&usqp=CAU',
-      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, recusandae?',
-    },
-    {
-      name: 'Pizza',
-      rating: 4.7,
-      image:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTooPlWSljxJsQv2hyv2JgVd-Ozk0lA4WmV2A&usqp=CAU',
-      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, recusandae?',
-    },
-    {
-      name: 'Pizza',
-      rating: 4.7,
-      image:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTooPlWSljxJsQv2hyv2JgVd-Ozk0lA4WmV2A&usqp=CAU',
-      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, recusandae?',
-    },
-    {
-      name: 'Pizza',
-      rating: 4.7,
-      image:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTooPlWSljxJsQv2hyv2JgVd-Ozk0lA4WmV2A&usqp=CAU',
-      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, recusandae?',
-    },
-    {
-      name: 'Pizza',
-      rating: 4.7,
-      image:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTooPlWSljxJsQv2hyv2JgVd-Ozk0lA4WmV2A&usqp=CAU',
-      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, recusandae?',
-    },
-    {
-      name: 'Pizza',
-      rating: 4.7,
-      image:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTooPlWSljxJsQv2hyv2JgVd-Ozk0lA4WmV2A&usqp=CAU',
-      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, recusandae?',
-    },
-    {
-      name: 'Pizza',
-      rating: 4.7,
-      image:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTooPlWSljxJsQv2hyv2JgVd-Ozk0lA4WmV2A&usqp=CAU',
-      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, recusandae?',
-    },
-    {
-      name: 'Pizza',
-      rating: 4.7,
-      image:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTooPlWSljxJsQv2hyv2JgVd-Ozk0lA4WmV2A&usqp=CAU',
-      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, recusandae?',
-    },
-    {
-      name: 'Pizza',
-      rating: 4.7,
-      image:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTooPlWSljxJsQv2hyv2JgVd-Ozk0lA4WmV2A&usqp=CAU',
-      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, recusandae?',
-    },
-  ];
-
+  reviews!: review[];
   stars: number[] = [1, 2, 3, 4, 5];
+  id!: String;
+  image =
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTooPlWSljxJsQv2hyv2JgVd-Ozk0lA4WmV2A&usqp=CAU';
 
-  num:number=0;
+  num: number = 0;
+
+  constructor(
+    private adminServiceAuth: AdminServiceService,
+    private adminAuth: AdminAuthService,
+    private location: Location,
+    private route: ActivatedRoute
+  ) {
+    if (this.location.path() === '/admin/dashboard/review') {
+      this.adminAuth.getAdmin().subscribe((data: adminProfile) => {
+        this.id = data._id;
+        this.adminServiceAuth.getReview(this.id).subscribe((res: review[]) => {
+          this.reviews = res;
+        });
+      });
+    } else {
+      this.route.params.subscribe((params) => {
+        this.id = params['id'];
+        this.adminServiceAuth.getReview(this.id).subscribe((res: review[]) => {
+          this.reviews = res;
+        });
+      });
+    }
+  }
+
   click(e: number) {
     this.num = e;
   }
 
-  constructor() {}
+  remove(id: String) {
+    this.adminServiceAuth.removeReview(id).subscribe((res) => {
+      console.log(res);
+      this.adminServiceAuth.getReview(this.id).subscribe((res: review[]) => {
+        this.reviews = res;
+      });
+    });
+  }
 
   ngOnInit(): void {}
 }
